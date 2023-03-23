aa23808fb91349139998ad48d1ee07058639e74a
George Wigley <georgewigley123@gmail.com>
2023-03-23 02:38:18 +0000

    update

--- blenderTest/pythonScript.py
+++ blenderTest/pythonScript.py
@@ -1,13 +1,13 @@
 import bpy
 
 # Add test plane
-#bpy.ops.mesh.primitive_plane_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+#bpy.ops.mesh.primitive_plane_add(enter_editmode=False, align='WORLD', location=(0, 0, 0), scale=(3, 3, 3))
 
 # Store ref to object
 #activeObj = bpy.context.active_object
 
 # Load demo file
-bpy.ops.import_scene.fbx(filepath="C:/Users/George.000/Desktop/blenderTest/inputs/testFootprint.fbx")
+bpy.ops.import_scene.fbx(filepath="C:/Users/George.000/Desktop/My project/blenderTest/inputs/testFootprint.fbx")
 
 # Store ref to object
 activeObj = bpy.context.selected_objects[0]
@@ -22,4 +22,4 @@ gnmod.node_group = bpy.data.node_groups['building']
 bpy.ops.object.duplicates_make_real(use_base_parent=True, use_hierarchy=True)
 
 # Export Mesh
-bpy.ops.export_scene.gltf(filepath="C:/Users/George.000/Desktop/blenderTest/outputs/test.gltf", export_format="GLTF_EMBEDDED", check_existing=False, use_selection=True)
+bpy.ops.export_scene.gltf(filepath="C:/Users/George.000/Desktop/My project/blenderTest/outputs/test.gltf", export_format="GLTF_EMBEDDED", check_existing=False, use_selection=True)
